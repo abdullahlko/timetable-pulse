aedc794eff52c9f983b1c2c126ba234e415032a5
Abdullah Ansari <abdullahlko789@gmail.com>
2026-03-03 05:05:56 +0530

    migrate Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhH1rvRqvv_6q2qY7SWE9sjqEd0GTgFQs",
-  authDomain: "timetable-pulse.firebaseapp.com",
-  projectId: "timetable-pulse",
-  storageBucket: "timetable-pulse.firebasestorage.app",
-  messagingSenderId: "1050684371197",
-  appId: "1:1050684371197:web:021bfd024a6350cc128e94",
-  measurementId: "G-6Z1VYDQBMR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 getAnalytics(app);
